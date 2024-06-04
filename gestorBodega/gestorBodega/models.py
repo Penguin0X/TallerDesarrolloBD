45ee019e2115bceba9f5764e377415c35328f6d1
ElTioMuffin <alvaro.toro08@inacapmail.cl>
2024-06-04 17:04:22 -0400

    Fix merge + configuracion djongo (Falta arreglar los modelos para que sean compatibles con mongoDB)

--- gestorBodega/gestorBodega/models.py
+++ gestorBodega/gestorBodega/models.py
@@ -1,14 +1,14 @@
 from djongo import models
+from django.core.validators import RegexValidator, MinLengthValidator
 
 class Rol(models.Model):
-    idRol= models.AutoField ()
+    
     nombreRol = models.CharField(max_length=100)
 
     def __str__(self):
         return self.nombreRol
 
 class Personal(models.Model):
-    idPersona = models.AutoField() 
     nombrePersonal = models.CharField(max_length=255)
     rolId = models.CharField(max_length=255)
     telefono = models.IntegerField(unique=True, validators=[RegexValidator(r'^[0-9]{8}$')])
@@ -19,16 +19,15 @@ class Personal(models.Model):
         return self.idPersona
 
 class Ubicacion(models.Model):
-    idUbicacion=models.AutoField
-    nombreUbicacion=models.CharField
-    descripcion=models.CharField
+    nombreUbicacion=models.CharField()
+    descripcion=models.CharField()
 
     def __str__(self):
         return self.idUbicacion
 
 class Stock(models.Model):
-    Ubicacion= models.CharField
-    cantidad= models.IntegerField
+    Ubicacion= models.CharField(max_length=100)
+    cantidad= models.IntegerField()
 
     def __str__(self):
         return f"Stock: {self.cantidad} unidades en {self.ubicacion.nombreUbicacion}"
@@ -40,31 +39,36 @@ class Stock(models.Model):
         self.cantidad += unidades_a_agregar
 
 class Estado(models.Model):
-    idEstado=models.AutoField
     nombreEstado=models.CharField(max_length=100)
 
     def __str__(self):
         return f"Estado: {self.nombreEstado}"
 
 class Consola(models.Model):
-    idConsola=models.AutoField
+    id = models.IntegerField(primary_key=True, default=None)
     NombreConsola = models.CharField(max_length=100)
     MarcaConsola = models.CharField(max_length=100)
 
 class Distribucion():
-    idDistribucion=models.AutoField
+    
     LocalidadDistribucion = models.CharField(max_length=100)
     siglaDistribucion = models.CharField(max_length=5)
 
+def __str__(self):
+        return self.nombreConsola
 
-class Juego(models.Model):    
+class Juego(models.Model):
+    
     codigoDeBarra = models.IntegerField(unique=True)
     nombreJuego = models.CharField(max_length=250)
-    consola = models.CharField (max_length=100)
-    distribucion = models.CharField(max_length=5)
-    estado = models.CharField(max_length=100)
-    stock = models.ManyToManyField
-    imagen=models.ImageField 
+    consola = models.EmbeddedField(model_container=Consola)
+    distribucion = models.CharField(max_length=5) #Region Juego
+    estado = models.CharField(max_length=100) #Estados (Descontinuado - Sin Stock - ETC)
+    unidades = models.EmbeddedField(model_container=Stock)
+    #cantidad
+ #ubicacion (Estante en bodega).
+    imagen=models.ImageField()
     
     def __str__(self):
         return self.nombreJuego
+
--- gestorBodega/gestorBodega/wsgi.py
+++ gestorBodega/gestorBodega/wsgi.py
@@ -15,21 +15,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestorBodega.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for gestorBodega project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestorBodega.settings')
-
-application = get_wsgi_application()
-
